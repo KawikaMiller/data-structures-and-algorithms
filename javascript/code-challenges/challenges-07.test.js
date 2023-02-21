3e8919c4b9cb91e472df261465a7e7241a78523f
KMART <93807261+KMArtwork@users.noreply.github.com>
2023-02-21 14:15:37 -0800

    complete challenges 1-4

--- javascript/code-challenges/challenges-07.test.js
+++ javascript/code-challenges/challenges-07.test.js
@@ -86,7 +86,7 @@ For example, wordsToCharList('gregor') returns ['g','r','e','g','o','r'].
 ------------------------------------------------------------------------------------------------ */
 
 const wordsToCharList = (arr) => {
-  // Solution code here...
+  
 };
 
 
