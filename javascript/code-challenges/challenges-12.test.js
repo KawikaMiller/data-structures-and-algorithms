77d33716ae6417d20c4317a787d5df99cd29a5b2
KMART <93807261+KMArtwork@users.noreply.github.com>
2023-02-28 15:28:04 -0800

    complete challenge 4, 5

--- javascript/code-challenges/challenges-12.test.js
+++ javascript/code-challenges/challenges-12.test.js
@@ -43,7 +43,7 @@ If it does, return true. If not, return false.
 ------------------------------------------------------------------------------------------------ */
 
 const hasNumber = (string) => {
-  // Solution code here...
+  return /[A-Za-z]+[0-9]+/.test(string)
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -63,7 +63,7 @@ Note: if you ever need to validate an email using a regex in practice, the Inter
 ------------------------------------------------------------------------------------------------ */
 
 const validateEmail = (email) => {
-  // Solution code here...
+  return /(\w+|\w+\.\w+)@\w+\.(com|net|org)/.test(email)
 };
 
 /* ------------------------------------------------------------------------------------------------
