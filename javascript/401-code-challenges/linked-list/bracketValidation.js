721c8b67dcc29c3dc28247e49c55433e2d8ff7c2
KMART <93807261+KMArtwork@users.noreply.github.com>
2023-04-19 18:05:29 -0700

    complete code challenge 401_13

--- javascript/401-code-challenges/linked-list/bracketValidation.js
+++ javascript/401-code-challenges/linked-list/bracketValidation.js
@@ -4,35 +4,46 @@ const Stack = require('./stacks');
 
 const bracketValidation = (string) => {
   let bracketStack = new Stack();
-  let curlyCount = 0;
-  let roundCount = 0;
-  let squareCount = 0;
-  [...string].forEach(char => {
-    if (char === '{') {
-      curlyCount++;
-    } else if (char === '}') {
-      curlyCount--;
-    } else if (char === '[') {
-      squareCount++
-    } else if (char === ']') {
-      squareCount--;
-    } else if (char === '(') {
-      roundCount++;
-    } else if (char === ')') {
-      roundCount--;
-    }
-  })
 
-  if (!curlyCount && !roundCount && !squareCount) {
+  // break the string into individual characters
+  let stringArr = [...string];
+  
+  // iterate through the array of string characters
+  for(const char of stringArr){
+    
+    // if the character is an opening bracket, push it into the stack
+    if (char.match(/\{|\[|\(/)) {
+      bracketStack.push(char)
+
+    // if the character is a close bracket, check to see if the stack is currently empty
+    } else if (char.match(/\}|\]|\)/)) {
+
+      // if the stack is currently empty and the next character is a closing bracket then we know that is an invalid way of using brackets so return false and stop the loop
+      if (bracketStack.isEmpty()) {
+        return false;
+      }
+
+      // get the top value of our stack
+      let openBracket = bracketStack.peek();
+
+      // compare top value of stack to current character, if they match bracket types then pop the top value from the stack
+      if (openBracket === '[' && char === ']') {
+        bracketStack.pop();
+      } else if(openBracket === '{' && char === '}') {
+        bracketStack.pop();
+      } else if(openBracket === '(' && char === ')') {
+        bracketStack.pop();
+      }
+    }
+  }
+
+  // if there are no more open brackets left in the stack, then we know that every opening bracket had a corresponding closing bracket and we know the string is valid and return true, if not it's invalid and return false
+  if (bracketStack.isEmpty()){
     return true
   } else {
     return false
   }
+
 }
 
-console.log(bracketValidation('[aaaaaaa]a(asdas){kjahsdkja}'));
-console.log(bracketValidation('[](){}'));
-console.log(bracketValidation('((()))'));
-console.log(bracketValidation('((())'));
-console.log(bracketValidation('{]})'));
-console.log(bracketValidation('{[}]'));
+module.exports = bracketValidation;
