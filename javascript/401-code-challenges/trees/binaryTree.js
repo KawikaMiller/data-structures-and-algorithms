9c47956ff037b9ab00c9f0bc3daee6b785ca1c22
KMART <93807261+KMArtwork@users.noreply.github.com>
2023-04-27 09:26:30 -0700

    maxValue method works

--- javascript/401-code-challenges/trees/binaryTree.js
+++ javascript/401-code-challenges/trees/binaryTree.js
@@ -37,7 +37,7 @@ class BinaryTree {
       valueArray = [...valueArray, ...this.inOrder(root.left)]
     };
 
-    valueArray = [...valueArray, ...root.value]
+    valueArray = [...valueArray, root.value]
 
     if (root.right !== null){
       valueArray = [...valueArray, ...this.inOrder(root.right)]
@@ -60,16 +60,16 @@ class BinaryTree {
     };
 
     // console.log(root.value)
-    return valueArray = [...valueArray, ...root.value]
+    return valueArray = [...valueArray, root.value]
 
   }
 
   maxValue = () => {
-    let values = this.preOrder(this.root);
+    let values = this.inOrder(this.root);
     let maxValue = 0;
-    values.forEach((value, idx) => {
-      if (value < values[idx+1]) {
-        maxValue = values[idx+1]
+    values.forEach(value => {
+      if (value > maxValue) {
+        maxValue = value;
       }
     })
     return maxValue;
@@ -96,23 +96,16 @@ class BinarySearchTree extends BinaryTree {
 
 }
 
-let myTree = new BinaryTree(1);
-myTree.root.left = new Node(2)
-myTree.root.left.left = new Node(3)
-myTree.root.left.right = new Node(5)
-myTree.root.right = new Node(4)
-myTree.root.right.left = new Node(2)
-console.log(myTree.maxValue())
+// let myTree = new BinaryTree(30);
+// myTree.root.left = new Node(9)
+// myTree.root.left.left = new Node(15)
+// myTree.root.left.right = new Node(20)
+// myTree.root.right = new Node(5)
+// myTree.root.right.left = new Node(7)
 
 // console.log(myTree.preOrder(myTree.root))
 // console.log(myTree.inOrder(myTree.root))
 // console.log(myTree.postOrder(myTree.root))
-
-
-// let BST = new BinarySearchTree('A');
-// BST.add('B');
-// BST.add('C');
-// console.log(BST.contains('D'))
-// console.log(BST.contains('A'))
+// console.log(myTree.maxValue());
 
 module.exports = {Node, BinaryTree, BinarySearchTree}
