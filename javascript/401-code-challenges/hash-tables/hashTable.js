ee1f4aad2367c47a6402229d6f56f936eb9ff8cb
KMART <93807261+KMArtwork@users.noreply.github.com>
2023-05-12 15:12:06 -0700

    keys method working

--- javascript/401-code-challenges/hash-tables/hashTable.js
+++ javascript/401-code-challenges/hash-tables/hashTable.js
@@ -61,7 +61,36 @@ class HashTable {
   }
 
   keys = () => {
+    let uniqueKeys = [];
 
+    this.buckets.forEach(bucket => {
+      //if bucket.head is truthy, then we know that it has a LinkedList as it's value
+      if(bucket.head){
+        // begin to traverse the linked list
+        let currentNode = bucket.head
+        
+        while(currentNode){
+          // assuming the value of our bucket.head is an object, it can potentially have more than one key:value pair
+          let bucketValueKeys = Object.keys(bucket.head.value);
+
+          // for every key, we check if that key already exists within the 'uniqueKeys' array
+          bucketValueKeys.forEach(key => {
+            // add the key to the 'uniqueKeys' array if it DOES NOT already exist within the array
+            if(!uniqueKeys.includes(key)){
+              uniqueKeys.push(key)
+            }
+          })
+          
+          // once we're done iterating through one node of a linkedlist, we check if there is another node following
+          if (currentNode.next) {
+            currentNode = currentNode.next;
+          } else break;
+        }
+
+      }
+    })
+
+    return uniqueKeys;
   }
 
   hash = (key) => {
@@ -84,7 +113,13 @@ let myHashTable = new HashTable(100);
 // console.log(myHashTable.hash('test'))
 // myHashTable.hash('test');
 myHashTable.set('test', 'value');
-myHashTable.set('test', 'value2');
-console.log(myHashTable.get('test'));
+myHashTable.set('test2', 'value2');
+myHashTable.set('apples', '2oranges');
+myHashTable.set('apples', '3oranges');
+myHashTable.set('apples', 'bananas');
+myHashTable.set('fefi', 'fofum');
+myHashTable.set('foo', 'bar');
+console.log(myHashTable.keys())
+// console.log(myHashTable.get('test'));
 // console.log(myHashTable.has('test'));
 // console.log(myHashTable.buckets[60].head)
