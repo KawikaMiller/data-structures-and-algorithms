28980f6e467bae33bb82de71d7ec5c058ea9d26b
KMART <93807261+KMArtwork@users.noreply.github.com>
2023-05-24 20:47:52 -0700

    update top level README

--- javascript/401-code-challenges/trees/binaryTree.js
+++ javascript/401-code-challenges/trees/binaryTree.js
@@ -59,7 +59,6 @@ class BinaryTree {
       valueArray = [...valueArray, ...this.postOrder(root.right)]
     };
 
-    // console.log(root.value)
     return valueArray = [...valueArray, root.value]
 
   }
@@ -98,5 +97,30 @@ class BinarySearchTree extends BinaryTree {
   }
 
 }
+
+// warm up 401 class 38
+const validateInOrderBST = (valueArray, rootValue) => {
+  let middle = valueArray.findIndex(value => value === rootValue);
+  let left = valueArray.slice(0, middle)
+  let right = valueArray.slice(middle + 1)
+
+  if (left.every(value => value < rootValue) && right.every(value => value > rootValue)) {
+    return true
+  } else return false;
+
+}
+
+// let myBST = new BinarySearchTree(5)
+// myBST.root.left = new Node(3)
+// myBST.root.left.left = new Node(1)
+// myBST.root.left.right = new Node(4)
+
+// myBST.root.right = new Node(6)
+// myBST.root.right.left = new Node(8)
+// myBST.root.right.right = new Node(10)
+
+// let valueArray = myBST.inOrder(myBST.root)
+// console.log(validateInOrderBST(valueArray, 2))
+
 
 module.exports = {Node, BinaryTree, BinarySearchTree}
