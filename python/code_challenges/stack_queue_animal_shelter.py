07ad29ec82434924a9e4fd3d664ab80373eada0c
KMART <93807261+KMArtwork@users.noreply.github.com>
2024-02-27 22:24:28 -0800

    complete animal shelter challenge

--- python/code_challenges/stack_queue_animal_shelter.py
+++ python/code_challenges/stack_queue_animal_shelter.py
@@ -1,13 +1,42 @@
 from data_structures.queue import Queue
+from data_structures.invalid_operation_error import InvalidOperationError
 
 
-class AnimalShelter:
-    pass
+class AnimalShelter(Queue):
+    def __init__(self):
+        super().__init__()
+
+    def enqueue(self, animal):
+        if animal.species != 'dog' and animal.species != 'cat':
+            raise InvalidOperationError('Shelter only accepts dogs &/o cats')
+        
+        super().enqueue(animal)
+
+    def dequeue(self, preference):
+        if self.is_empty() == True:
+            raise InvalidOperationError('Shelter is empty')
+        else:
+            bq = Queue()
+
+            while self.is_empty() == False:
+                if self.peek().species == preference or preference == None:
+                    return super().dequeue()
+                else:
+                    bq.enqueue(super().dequeue())
+
+            self.back = bq.front
 
 
-class Dog:
-    pass
+class Animal:
+    def __init__(self, species):
+        self.species = species
 
 
-class Cat:
-    pass
+class Dog(Animal):
+    def __init__(self):
+        super().__init__('dog')
+
+
+class Cat(Animal):
+    def __init__(self):
+        super().__init__('cat')
